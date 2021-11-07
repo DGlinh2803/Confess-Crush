cfdb6bc4d171c0ad44b9f5ecd82e06ce44b8354e
DGlinh2803 <93864368+DGlinh2803@users.noreply.github.com>
2021-11-07 18:30:22 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "Anh Yêu Em",
+    introTitle: 'Công chúa Hân',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
@@ -12,9 +12,9 @@ const CONFIG = {
     btnNo: 'Không nha :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
+    reply: 'Tại anh đẹp trai vcl và anh rất yêu em nữa',
+    mess: 'Anh biết mà 🥰. Yêu em rất  nhiều nhiều 😘😘',
+    messDesc: 'Hãy ở bên anh đến cuối đời nhé yêu em lắm ',
+    btnAccept: 'Dạ em cx yêu anh lắm ',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
